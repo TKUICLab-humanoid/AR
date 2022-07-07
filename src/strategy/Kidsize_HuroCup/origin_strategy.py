0b95e7d36b589b73d49db4a0839f01e13724f81b
hurocup_backup <humanoidgithub@gmail.com>
2022-07-07 19:26:26 +0800

    motion 1.45

--- src/strategy/Kidsize_HuroCup/origin_strategy.py
+++ src/strategy/Kidsize_HuroCup/origin_strategy.py
@@ -49,26 +49,27 @@ def Find_Target():
     for j in range (send.color_mask_subject_cnts[2]):
         for k in range (send.color_mask_subject_cnts[1]):
             for m in range (send.color_mask_subject_cnts[5]):
-                if   50 < send.color_mask_subject_Width[2][j] < 150 and 50 < send.color_mask_subject_Height[2][j] < 150 :
-                    if  5 < send.color_mask_subject_Width[1][k] <90 and 5 < send.color_mask_subject_Height[1][k] < 90:
-                        if  0 < send.color_mask_subject_Width[5][m]< 25 and 0 < send.color_mask_subject_Height[5][m] < 25:
-                                bcolor_XMin = send.color_mask_subject_XMin[2][j]
-                                bcolor_XMax = send.color_mask_subject_XMax[2][j]
-                                bcolor_YMin = send.color_mask_subject_YMin[2][j]
-                                bcolor_YMax = send.color_mask_subject_YMax[2][j]
-                                ycolor_XMin = send.color_mask_subject_XMin[1][k]
-                                ycolor_XMax = send.color_mask_subject_XMax[1][k]
-                                ycolor_YMin = send.color_mask_subject_YMin[1][k]
-                                ycolor_YMax = send.color_mask_subject_YMax[1][k] 
-                                rcolor_XMin = send.color_mask_subject_XMin[5][m]
-                                rcolor_XMax = send.color_mask_subject_XMax[5][m]
-                                rcolor_YMin = send.color_mask_subject_YMin[5][m]
-                                rcolor_YMax = send.color_mask_subject_YMax[5][m]
-                                rcolor_X = send.color_mask_subject_X[5][m]
-                                rcolor_Y = send.color_mask_subject_Y[5][m]
-    #  這邊有修改                # send.drawImageFunction(0,1,bcolor_XMin,bcolor_XMax,bcolor_YMin,bcolor_YMax,0,0,255)
-                                # send.drawImageFunction(1,1,ycolor_XMin,ycolor_XMax,ycolor_YMin,ycolor_YMax,0,0,255)
-                                send.drawImageFunction(2,1,rcolor_XMin,rcolor_XMax,rcolor_YMin,rcolor_YMax,0,0,255)
+                if -2 <= send.color_mask_subject_X[2][j] - send.color_mask_subject_X[1][k] <=2 and -2 <= send.color_mask_subject_Y[2][j] - send.color_mask_subject_Y[1][k] <=2:
+                    if -2 <= send.color_mask_subject_X[1][k] - send.color_mask_subject_X[5][m] <= 2 and  -2 <= send.color_mask_subject_Y[1][k] - send.color_mask_subject_Y[5][m] <= 2 :
+                # if   send.color_mask_subject_Width[2][j] - send.color_mask_subject_Width[1][k]>0  and send.color_mask_subject_Height[2][j] - send.color_mask_subject_Height[1][k] >0 :
+                #     if   send.color_mask_subject_Width[1][k] - send.color_mask_subject_Width[5][m] >0 and send.color_mask_subject_Height[1][k] -send.color_mask_subject_Height[5][m] >0 :
+                        bcolor_XMin = send.color_mask_subject_XMin[2][j]
+                        bcolor_XMax = send.color_mask_subject_XMax[2][j]
+                        bcolor_YMin = send.color_mask_subject_YMin[2][j]
+                        bcolor_YMax = send.color_mask_subject_YMax[2][j]
+                        ycolor_XMin = send.color_mask_subject_XMin[1][k]
+                        ycolor_XMax = send.color_mask_subject_XMax[1][k]
+                        ycolor_YMin = send.color_mask_subject_YMin[1][k]
+                        ycolor_YMax = send.color_mask_subject_YMax[1][k] 
+                        rcolor_XMin = send.color_mask_subject_XMin[5][m]
+                        rcolor_XMax = send.color_mask_subject_XMax[5][m]
+                        rcolor_YMin = send.color_mask_subject_YMin[5][m]
+                        rcolor_YMax = send.color_mask_subject_YMax[5][m]
+                        rcolor_X = send.color_mask_subject_X[5][m]
+                        rcolor_Y = send.color_mask_subject_Y[5][m]
+                        # send.drawImageFunction(0,1,bcolor_XMin,bcolor_XMax,bcolor_YMin,bcolor_YMax,0,0,255)#  這邊有修改
+                        # send.drawImageFunction(1,1,ycolor_XMin,ycolor_XMax,ycolor_YMin,ycolor_YMax,0,0,255)
+                        send.drawImageFunction(2,1,rcolor_XMin,rcolor_XMax,rcolor_YMin,rcolor_YMax,0,0,255)
                                 #print(rcolor_X,rcolor_Y) 
  
     return rcolor_X,rcolor_Y
@@ -84,19 +85,20 @@ def Low_xy(i):
     for f in range (send.color_mask_subject_cnts[2]):
         for g in range (send.color_mask_subject_cnts[1]):
             for h in range (send.color_mask_subject_cnts[5]):
-                if   50 < send.color_mask_subject_Width[2][f] < 150 and 50 < send.color_mask_subject_Height[2][f] < 150 :
-                    if  5 < send.color_mask_subject_Width[1][g] <90 and 5 < send.color_mask_subject_Height[1][g] < 90:
-                        if  0 < send.color_mask_subject_Width[5][h]< 25 and 0 < send.color_mask_subject_Height[5][h] < 25:
-                            l_XMin = send.color_mask_subject_XMin[i][h]
-                            l_XMax = send.color_mask_subject_XMax[i][h]
-                            l_YMin = send.color_mask_subject_YMin[i][h]
-                            l_YMax = send.color_mask_subject_YMax[i][h]       
-                            if send.color_mask_subject_Y[i][h] >= Y_low:
-                                Y_low = send.color_mask_subject_Y[i][h]
-                                X_low = send.color_mask_subject_X[i][h]
-                # time.sleep(0.4)
-                                send.drawImageFunction(3,1,l_XMin,l_XMax,l_YMin,l_YMax,255,48,48) 
-                                time.sleep(0.27) #修改====================================================================================================================
+                if -2 <= send.color_mask_subject_X[2][f] - send.color_mask_subject_X[1][g] <=2 and -2 <= send.color_mask_subject_Y[2][f] - send.color_mask_subject_Y[1][g] <=2:
+                    if -2 <= send.color_mask_subject_X[1][g] - send.color_mask_subject_X[5][h] <= 2 and  -2 <= send.color_mask_subject_Y[1][g] - send.color_mask_subject_Y[5][h] <= 2 :
+                # if   send.color_mask_subject_Width[2][f] - send.color_mask_subject_Width[1][g]>0  and send.color_mask_subject_Height[2][f] - send.color_mask_subject_Height[1][g] >0 :
+                #     if   send.color_mask_subject_Width[1][g] - send.color_mask_subject_Width[5][h] >0 and send.color_mask_subject_Height[1][g] -send.color_mask_subject_Height[5][h] >0 :
+                        l_XMin = send.color_mask_subject_XMin[5][h]
+                        l_XMax = send.color_mask_subject_XMax[5][h]
+                        l_YMin = send.color_mask_subject_YMin[5][h]
+                        l_YMax = send.color_mask_subject_YMax[5][h]       
+                        if send.color_mask_subject_Y[i][h] >= Y_low:
+                            Y_low = send.color_mask_subject_Y[5][h]
+                            X_low = send.color_mask_subject_X[5][h]
+            # time.sleep(0.4)
+                            send.drawImageFunction(3,1,l_XMin,l_XMax,l_YMin,l_YMax,255,48,48) 
+                            time.sleep(0.27) #修改====================================================================================================================
                                   
     return X_low ,Y_low
 
@@ -139,6 +141,10 @@ if __name__ == '__main__':
         hh = 0
         r = rospy.Rate(5)
         k = 0
+        hl = 0
+        lll =0
+        hll = 0
+        hlll = 0
         while not rospy.is_shutdown():
             send.drawImageFunction(4,0,0,320,120,120,0,0,0)
             send.drawImageFunction(5,0,160,160,0,240,0,0,0)
@@ -147,8 +153,9 @@ if __name__ == '__main__':
                  
                 if HH == True:
                 #     hh = 3123
-                    send.sendHeadMotor(1,2805,50)
-                    time.sleep(5)
+                    time.sleep(0.5)
+                    send.sendHeadMotor(1,2805,80)
+                    time.sleep(4)
                 #     if hh == 3123:
                     HH = False 
 
@@ -172,24 +179,47 @@ if __name__ == '__main__':
                         Y_low = 0 
 
                     print("現在Y值： ===============",Low_xy(5)[1])
-                    if -10<= Low_xy(5)[0] - 160 <= 10:
-                        if Low_xy(5)[1] > 130 :
-                            send.sendBodySector(5)
-                            print("LOW")
-                            X_low = 0
-                            Y_low = 0 
-                            time.sleep(2)
+                    if hlll == 0:
+                        if -10<= Low_xy(5)[0] - 160 <= 10:
+                            if Low_xy(5)[1] > 142 :
+                                hl = Low_xy(5)[1] - 142
+                                print(hl)
+                                if hl >= 13:
+                                    hl = 13
+                                
 
-                    if -10<= Low_xy(5)[0] - 160 <= 10:
-                        if Low_xy(5)[1] <130 :
-                            send.sendBodySector(4)
-                            print("HIGH")
-                            X_low = 0
-                            Y_low = 0 
-                            time.sleep(2)
+                                for hll in range(0,hl) :
+                                    send.sendBodySector(5)
+                                    time.sleep(0.5)
+                                    print("LOW")
+                                    i = i+ 1
+                                
+                            
+                                X_low = 0
+                                Y_low = 0 
+                                i = 0
+                                hlll = hlll+1
+                                
+
+                        if -10<= Low_xy(5)[0] - 160 <= 10:
+                            if 120 <Low_xy(5)[1] <142 :
+                                hl =142 -  Low_xy(5)[1] 
+                                print(hl)
+                                if hl >= 3:
+                                    hl = 3
+                                for hll in range(0,hl) :
+                                    send.sendBodySector(4)
+                                    time.sleep(0.5)
+                                    print("HIGH")
+                                    i = i+ 1
+                                X_low = 0
+                                Y_low = 0 
+                                i = 0
+                                hlll = hlll+1
+                            
 
                     
-                    if -10<= Low_xy(5)[0] - 160 <= 10 and Low_xy(5)[1] == 130 :
+                    if -10<= Low_xy(5)[0] - 160 <= 10 and -10 <= Low_xy(5)[1] -142 <= 10 :
                         print("time start")
                         
                         all()
@@ -209,14 +239,13 @@ if __name__ == '__main__':
                             
                             print("\n新的最低點XY是：",l)
                             print("\n轉腰的數值是：",i)
-                            if g >= 1.757:    #比賽3.667
-                                h = g - 1.757
-                            if g<1.757:
-                                h = g*2 -1.757
+                            if g >= 1.42:    #比賽3.667
+                                h = g - 1.42
+                            if g<1.42:
+                                h = g*2 -1.42#數值加大 箭矢往右邊
                             print(h)
                             start = end
-                            X_low = 0
-                            Y_low = 0
+                            
                             
                             print(h)
                             v = False
@@ -244,6 +273,8 @@ if __name__ == '__main__':
                         
                         g = 0
                         k = 0
+                        X_low = 0
+                        Y_low = 0
                         
                        # if g >= 3 :
                        #     send.sendBodySector(1) #動作串198 射擊   
