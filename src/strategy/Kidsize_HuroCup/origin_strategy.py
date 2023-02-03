70edbc872a8d0d58266a1917e6de8af7cce98866
hurocup_white <humanoidgithub@gmail.com>
2023-02-03 15:26:53 +0800

    update strategy test strategy motion test

--- src/strategy/Kidsize_HuroCup/origin_strategy.py
+++ src/strategy/Kidsize_HuroCup/origin_strategy.py
@@ -5,6 +5,7 @@ import numpy as np
 from Python_API import Sendmessage
 import time
 import timeit
+import math
 bcolor_XMin = 0
 bcolor_XMax = 0
 bcolor_YMin = 0
@@ -29,8 +30,11 @@ z = True
 y = True
 l = 0
 tt = 0
-HH = True
-
+power = True
+#sendBodySector(1) 向右轉
+#sendBodySector(2) 向左轉
+#sendBodySector(3) 向下轉
+#sendBodySector(4) 向上轉
 def Find_Target():
     global bcolor_XMin 
     global bcolor_XMax 
@@ -71,7 +75,9 @@ def Find_Target():
                         # send.drawImageFunction(1,1,ycolor_XMin,ycolor_XMax,ycolor_YMin,ycolor_YMax,0,0,255)
                         send.drawImageFunction(2,1,rcolor_XMin,rcolor_XMax,rcolor_YMin,rcolor_YMax,0,0,255)
                                 #print(rcolor_X,rcolor_Y) 
- 
+                else :
+                    rcolor_Y=0
+                    rcolor_X=0
     return rcolor_X,rcolor_Y
     
 def Low_xy(i):
@@ -102,11 +108,6 @@ def Low_xy(i):
                                   
     return X_low ,Y_low
 
-def all():
-    Find_Target()
-    #time.sleep(0.4)
-    Low_xy(5)
-
 imagedata = [[None for H in range(240)]for W in range(320)]
 
 def TS_time(i):
@@ -132,282 +133,102 @@ def TS_time(i):
 if __name__ == '__main__':
     try:
         send = Sendmessage()
-        m = 0
-        mi = 0
-        msum = 0
-        startM = 0
-        endM = 0
-        g = 0
-        h = 0
-        i = 0
-        hh = 0
-        hhll = 0
-        r = rospy.Rate(5)
-        k = 0
-        hl = 0
-        lll =0
-        hll = 0
-        hlll = 0
-        rh = 0
-        rlh = 0
-        rll = 0
-        rr = 0
-        rrl = 0
-        rrll = 0
-        endd = 0
-        lowy = 0
-        hlltr = True
-        DIO = True
-        while not rospy.is_shutdown():
+        point_x=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]#全部點的x座標
+        point_y=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]#全部點的y座標
+        i=0#計數用
+        j=0#計算目前總共紀錄了幾個點
+        k=0#no_point在預設區間出現的個數
+        x=0#要動多少
+        y=0
+        z=0
+        w=0
+        x_diff=0#計算瞄準點與目標點的x差距
+        y_diff=0#計算瞄準點與目標點的y差距
+        waist_move=0
+        hand_move=0
+        target_point_x=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]#真正有值的點x座標存放處
+        target_point_y=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]#真正有值的點y座標存放處
+        while not rospy.is_shutdown():#劃出起始十字
             send.drawImageFunction(4,0,0,320,120,120,0,0,0)
             send.drawImageFunction(5,0,160,160,0,240,0,0,0)
             
-            if send.is_start == True :
+            if send.is_start == True :#啟動電源與擺頭
                 
-                if HH == True:
+                if power == True:
                     time.sleep(0.5)
-                    send.sendHeadMotor(1,2816,80)
-                    send.sendHeadMotor(1,2816,80)
-                    # send.sendSingleMotor(9,10,15) #啟動轉腰
-                    time.sleep(3)
-                    HH = False 
+                    power = False
 
-                all() 
-                s = Low_xy(5)  
                 
-                if  -10 <= s[0]-Find_Target()[0] <= 10 and endd == 0  :
+                    while j<90 and i<20:#抓出每一個點
+                        Find_Target()
+                        if Find_Target()[0]!=0 or Find_Target()[1]!=0:
+                            point_x[j]=Find_Target()[0]
+                            point_y[j]=Find_Target()[1]
+                            i=i+1
+                            j=j+1
+                            time.sleep(0.25)
+                        if Find_Target()[0]==0 or Find_Target()[1]==0:
+                            point_x[j]=0
+                            point_y[j]=0
+                            j=j+1
+                            time.sleep(0.25)
 
-                    all()   
-                    print('X軸差距 = ============',Low_xy(5)[0] - 160)  
-                    print('Y軸差距 = ============',Low_xy(5)[1] - 120) 
-                    
-                    if Low_xy(5)[0] - 160 > 1 or 160 - Low_xy(5)[0]>1: #and  Low_xy(5)[1]>120:
-                        m = 160 - Low_xy(5)[0] 
-                        mi = m*3 
-                        msum = msum + mi
-                        print(msum)
-                        if msum > 800 or msum < -800:
-                            print("暴轉========================================================================================================================================================")
-                            time.sleep(2)
-                            send.sendSingleMotor(9,int(-800),10)
-                            Low_xy(5)
-                            Find_Target()
-                            msum = 0
-                            time.sleep(5)
+                    i=0
+                    while i<90 and z<20:#將全部的點開始做過濾
+                        if point_x[i]!=0 or point_y[i]!=0:
+                            target_point_x[z]=point_x[i]
+                            target_point_y[z]=point_y[i]
+                            print(target_point_x[z],target_point_y[z])
+                            z=z+1
+                            i=i+1
+                        elif point_x[i]==0 or point_y[i]==0:
+                            print("沒點")
+                            if 17>z>4:
+                                if point_x[i]==0 or point_y[i]==0:
+                                    k=k+1
+                            i=i+1
 
-
-   
-                        
-                        
-                         
-                        send.sendSingleMotor(9,int(m*3),15)
-                        time.sleep(3) 
-                        X_low = 0
-                        Y_low = 0 
-
-                    print("現在Y值： ===============",Low_xy(5)[1])
-
-
- 
-                    lowy = 141                   #最低點高低改這裡
-                    if hlll == 0:
-                        if -1<= Low_xy(5)[0] - 160 <= 1:
-                            if Low_xy(5)[1] > lowy:           #改151
-                                hl = Low_xy(5)[1] - lowy
-                                
-                                print(hl)
-                                if hl >= 13:
-                                    hl = 13
-                                hhll = hhll - hl 
-                                if hll < hl and hlltr == True: 
-                                    for hll in range(0,hl) :
-                                        send.sendBodySector(5)
-                                        time.sleep(1)
-                                        print("LOW")
-                                        hll = hll+ 1
-                                        rll = rll+1
-                                        if hll == 13 :
-                                            hlltr = False
-
-
-
-                                
-                                    # for hll in range(0,hhll) :
-                                    #     send.sendBodySector(10)
-                                    #     print("hand LOW")
-                                    #     hll = hll+ 1
-                                    #     rh = rh +1
-                                    #     time.sleep(0.5)
-
-                                
-                            
-                                X_low = 0
-                                Y_low = 0 
-                                i = 0
-                                hlll = hlll+1
-                                
-
-                        if -1<= Low_xy(5)[0] - 160 <= 1:
-                            if Low_xy(5)[1] <lowy :
-                                hl =lowy -  Low_xy(5)[1]
-                                hhll = hl
-
-                                print(hl)
-                                if hl >= 3:
-                                    hl = 3
-                                    hhll = hhll - hl 
-                                    for hll in range(0,hhll) :
-                                        send.sendBodySector(7)
-                                        print("hand high")
-                                        hll = hll+ 1
-                                        rh = rh +1
-                                        time.sleep(0.5)
-
-                                    
-                                for hll in range(0,hl) :
+                    print(k)
+                    x_diff=target_point_x[16]-160#計算目標點與中心點的差距
+                    y_diff=target_point_y[16]-162
+                    waist_move=round(x_diff/9)
+                    hand_move=round(y_diff/2)
+                    print(waist_move,hand_move)
+                    while w==0:#持續追蹤靶
+                        Find_Target()
+                        time.sleep(0.25)
+                        now_target=Find_Target()
+                        if -15<now_target[0]-target_point_x[3]<15 and -15<now_target[1]-target_point_y[3]<15:#判斷通過基準點，我們設定是第4個點
+                            print("i find you!")
+                            if waist_move>0:#當x_diff 的值是正的，就正常向右轉
+                                for x in range (0,waist_move):
+                                    send.sendBodySector(1)
+                                    print("向右轉")
+                                time.sleep(0.1)
+                                waist_move=0
+                            if waist_move<0:#當x_diff 的值是負的，得先將x_diff變成正的，再用迴圈向左轉
+                                waist_move=waist_move*(-1)
+                                for x in range (0,waist_move):
+                                    send.sendBodySector(2)
+                                    print("向左轉")
+                                time.sleep(0.1)
+                                waist_move=0
+                            if hand_move>0:#當y_diff 的值是正的，就正常機體向下
+                                for x in range (0,hand_move):
+                                    send.sendBodySector(3)
+                                    print("向下")
+                                time.sleep(0.1)
+                                hand_move=0
+                            if hand_move<0:#當y_diff 的值是負的，得先將y_diff變成正的，再用迴圈向上
+                                hand_move=hand_move*(-1)
+                                for x in range (0,hand_move):
                                     send.sendBodySector(4)
-                                    time.sleep(1)
-                                    print("HIGH")
-                                    rlh = rlh +1
-                                    hll = hll+ 1
-                                X_low = 0
-                                Y_low = 0 
-                                i = 0
-                                hlll = hlll+1
-                            
+                                    print("向上")
+                                time.sleep(0.5)
+                                hand_move=0
 
-                    
-                    if -1<= Low_xy(5)[0] - 160 <= 1:# and -10 <= Low_xy(5)[1] -162 <= 10 :
-                        print("time start")
-                        
-                        all()
-                        TS_time(5)
-                        endM = TS_time(5)[1]
-                        startM = TS_time(5)[0]                          
-                        if endM -startM >= 0 and y == True:       
-                            y = False     
-                        all()
-                        if -10 <= Low_xy(5)[0] - 160 <= 10  and z== True:#and Low_xy(5)[1]>120
-                            l =  Low_xy(5)  
-                            z = False  
-                        
-                        if y==False and z== False:
-                            print("\n測量時間：%f 秒" % (endM -startM ))
-                            g = endM - startM
-                            
-                            print("\n新的最低點XY是：",l)
-                            print("\n轉腰的數值是：",mi)
 
-                            if g >= 1.42:   
-                                h = g - 1.42
-                            if g<1.42:
-                                h = g*2 -1.42    #數值加大 箭矢往右邊
-                            print(h)
-                            start = end
-                            
-                            
-                            print(h)
-                            v = False
-                            y = True
-                            z = True
-                            
-                            # if h <= 0:
-                            #     h = 1  
-                            all()
-                            time.sleep(1.5)
-                            if -1 <= Low_xy(5)[0] - 160 <= 1 and h != 0 and endd == 0:
-                                print(h)
-                                    
-                                #time.sleep(h)
-                                print("射擊")
-                                print(hhll)
-                                print(hl)
-                                if send.DIOValue == 25:
-                                    # send.sendBodySector(10)
-                                    # time.sleep(2)
-
-                                    send.sendSingleMotor(9,int(-hhll),10)
-                                    time.sleep(2)
-                                    send.sendSingleMotor(9,int(-hl),10)
-                                    print("轉了：", int(-hhll*0.45))
-                                    time.sleep(5)
-                                    DIO = True
-                                send.sendBodySector(3)
-                                time.sleep(2)
-                                print("aaaaaaaaaaaaaaaaaaaaa")
-                                lll = 1
-                                endd = 1
-                                print ("總轉腰數值 ：",msum)
-                                
-                                #send.is_start = False
-                            
-
-            if send.is_start == False : 
-                print("HHHHHHHH")
-                if lll == 1:
-                    send.sendSingleMotor(9,int(-1*msum),15)
-                    send.sendHeadMotor(1,2048,40)
-                    time.sleep(2)
-                    send.sendBodySector(2)
-                    time.sleep(1)
-                    for rr in range(0,rlh) :
-                        send.sendBodySector(5)
-                        time.sleep(1)
-                        
-                        rr = rr +1
-                    time.sleep(2)
-
-                    for rrl in range(0,rh) :
-                        send.sendBodySector(8)
-                        time.sleep(0.5)
-                        
-                        rrl = rrl +1
-
-                    time.sleep(2)
-                    for rrll in range(0,rll) :
-                        send.sendBodySector(4)
-                        time.sleep(1)
-                        
-                        rrll = rrll +1
-                    time.sleep(2)
-                    if DIO == True:
-                        send.sendSingleMotor(9,int(hhll),15)
-                        send.sendSingleMotor(9,int(hl),10)
-                        DIO = False
-                    lll = 0
-
-                
-
-                        
-                        # g = 0
-                        # k = 0
-                        # X_low = 0
-                        # Y_low = 0
-                        
-                       # if g >= 3 :
-                       #     send.sendBodySector(1) #動作串198 射擊   
-            r.sleep()
+            if send.is_start == False :
+                print("lkj is idiot")
     except rospy.ROSInterruptException:
         pass
-
-#加上轉頭 完成
-#修改不要直接跳出 完成
-#轉腰的最大範圍 完成
-# AR2022.ini motion
-# 212:站姿
-# 400:暫時測試(站平）
-
-#初始：212->201->200->199->400
-#9:蹲下 128:起
-#198(射擊）:11->143->11->143->110->72->144
-#198(new):11->11->110->72->144
-#100:100 300夾
-#m6:270 m2:-180
-
-# 99 要修改
-
-
-
-
-#左手往上-33 右手1次10
-#3號馬達選轉6*2次
