59453dbdbbd9d24c7003bc9200e3ff9d76892f68
aa <w1649n@gmail.com>
2024-07-16 13:19:15 +0800

    7/16 peepee revise x_branchmark

--- ar.py
+++ ar.py
@@ -12,8 +12,8 @@ HEAD_CHECK = 2080
 HAND_BACK = 222
 LEG_BACK = 1812
 VERTICAL_HEAD = 2048
-X_BENCHMARK = [229, 229, 229, 233, 232] #改大射左 #[最左,中左,中間,中右,最右]
-Y_BENCHMARK =140 #改大射高
+X_BENCHMARK = [223, 224, 227, 224, 223] #改大射左 #[最左,中左,中間,中右,最右]
+Y_BENCHMARK =133 #改大射高
 SHOOT_DELAY = 0.68 #改大變快
 
 #motion sector
@@ -196,7 +196,7 @@ class Archery:
                 if 0 < self.lowest_x <= 85: #最右
                     self.x_benchmark_type = 4#改大射左
                     print("444444444444444444444")
-                elif 85 < self.lowest_x <= 105: #中右
+                elif 85 < self.lowest_x <= 105: #中右  
                     self.x_benchmark_type = 3
                     print("333333333333333333333")
                 elif self.lowest_x >= 180: #最左
